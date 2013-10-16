562b7ed3c4b3569912d4eea63a5038b5c4264721
Wirawan Adi <oewank.blue@gmail.com>
2013-10-16 16:05:27 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-mercury
+Mercury
 =======
+
+This project purpose was to help me (obviously) and the other who wants to learn about JSF 2.2x, JEE 7 and other bleeding edge enterprise technologies.
---
+++ mercury-domain/src/main/java/me/awnkblz/mercury/domain/MUser.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2013 Wirawan.Adi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.awnkblz.mercury.domain;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Wirawan.Adi
+ */
+@Entity
+@Table(schema = "public", name = "MUser", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"user_name"})
+})
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Muser.findActiveUsers", query = "SELECT user FROM MUser AS user WHERE user.status = TRUE")})
+public class MUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "employee_name")
+    private String employeeName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "status")
+    private Boolean status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof MUser)) {
+            return false;
+        }
+        MUser other = (MUser) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "me.awnkblz.mercury.domain.MUser[ id=" + id + " ]";
+    }
+}
